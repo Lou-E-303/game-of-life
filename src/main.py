36ceafe9e934b2172b4547d18058d16752575aba
Lou-E-303 <louiscowell404@gmail.com>
2021-08-19 09:24:40 +0100

    Refactor set_square function with proper try/except

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ columns = 8
 board = utils_board.setup_board(rows, columns)
 
 utils_board.print_board(board)
-utils_board.set_square(board, 0, 0, True)
+utils_board.set_square(board, 9, 0, True)
 utils_board.print_board(board)
--- src/utils_board.py
+++ src/utils_board.py
@@ -9,13 +9,12 @@ def print_board(board):
     print("\n")
 
 
-# TODO: Replace if statement with proper Python try/except
-#  as this is currently a bit messy
-
 def set_square(board, row_index, column_index, alive_or_dead):
-    if (row_index <= len(board)) and (column_index <= len(board[0])):
+    try:
         board[row_index][column_index] = 1 if alive_or_dead else 0
-        return board
-    else:
+    except IndexError:
         print("Index out of bounds: Square (" + str(row_index) + ", " + str(column_index) + ") not set")
-        return board
+
+    return board
+
+
