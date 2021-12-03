3a26143c4437074846f738a01ff07b4badf7cd27
Lou-E-303 <louiscowell404@gmail.com>
2021-12-03 11:04:37 +0000

    Minor change for neatness

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import utils_board
 import utils_simulation
 
-rows = 8
-columns = 8
+rows, columns = 8, 8
+
 board = utils_board.setup_board(rows, columns)
 
 board = utils_simulation.set_initial_position(board)
